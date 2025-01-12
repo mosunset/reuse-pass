6fbabe2db96ff0202530ba0e7a3c75145dc6ece0
drive <driveasano@gmail.com>
2025-01-12 22:05:36 +0900

    mypage midway

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,27 @@
+import { redirect } from 'next/navigation';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { createClient } from '@/utils/supabase/server';
+
+const Page = async () => {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase.auth.getUser();
+    if (error || !data?.user) {
+        redirect('/login');
+    }
+    return (
+        <main className="container p-4">
+            <h1>My Page</h1>
+            <Avatar>
+                <AvatarImage
+                    src=""
+                    alt={data.user.email}
+                />
+                <AvatarFallback>{data.user.email?.slice(0, 2)}</AvatarFallback>
+            </Avatar>
+            <div>{data.user.email}</div>
+        </main>
+    );
+};
+
+export default Page;
