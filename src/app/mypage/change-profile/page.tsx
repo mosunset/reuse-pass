451fe625d266657daf28e3c7e18983f4aa8f6676
drive <driveasano@gmail.com>
2025-01-13 17:38:44 +0900

    mypage

---
+++ src/app/mypage/change-profile/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <main className="container p-4">
+            <h1 className="mb-4 w-full text-center text-2xl">
+                プロフィール変更
+            </h1>
+            <div></div>
+        </main>
+    );
+};
+
+export default Page;
--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
 import { createClient } from '@/utils/supabase/server';
 import SignOutButton from './_components/sign-out-button';
 
@@ -10,19 +12,60 @@ const Page = async () => {
     if (error || !data?.user) {
         redirect('/login');
     }
+
     return (
         <main className="container p-4">
-            <h1>My Page</h1>
-            <Avatar>
-                <AvatarImage
-                    src=""
-                    alt={data.user.email}
-                />
-                <AvatarFallback>{data.user.email?.slice(0, 2)}</AvatarFallback>
-            </Avatar>
-            <div>{data.user.email}</div>
-            <div>
-                <SignOutButton />
+            <h1 className="mb-4 w-full text-center text-2xl">My Page</h1>
+            <div className="flex items-center space-x-4">
+                <Avatar>
+                    <AvatarImage
+                        src=""
+                        alt={data.user.email}
+                    />
+                    <AvatarFallback>
+                        {data.user.email?.slice(0, 2)}
+                    </AvatarFallback>
+                </Avatar>
+                <div>{data.user.email}</div>
+            </div>
+            <div className="[&>div]:my-4">
+                <div>
+                    <Button
+                        variant={'outline'}
+                        asChild
+                    >
+                        <Link href={'/mypage/change-profile'}>
+                            プロフィール変更
+                        </Link>
+                    </Button>
+                </div>
+                <div>
+                    <Button
+                        variant={'outline'}
+                        asChild
+                    >
+                        <Link href={''}>出品物一覧</Link>
+                    </Button>
+                </div>
+                <div>
+                    <Button
+                        variant={'outline'}
+                        asChild
+                    >
+                        <Link href={''}>購入履歴</Link>
+                    </Button>
+                </div>
+                <div>
+                    <Button
+                        variant={'outline'}
+                        asChild
+                    >
+                        <Link href={''}>プレミアム課金</Link>
+                    </Button>
+                </div>
+                <div>
+                    <SignOutButton />
+                </div>
             </div>
         </main>
     );
