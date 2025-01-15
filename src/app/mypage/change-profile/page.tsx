13216899bab1e51eac6318320eb752c9dd945f5f
drive <driveasano@gmail.com>
2025-01-15 22:19:16 +0900

    プロフィール変更ページのレイアウトを改善し、コンテナクラスを削除

--- src/app/mypage/change-profile/page.tsx
+++ src/app/mypage/change-profile/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <main className="container p-4">
+        <main className="p-4">
             <h1 className="mb-4 w-full text-center text-2xl">
                 プロフィール変更
             </h1>
--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -14,7 +14,7 @@ const Page = async () => {
     }
 
     return (
-        <main className="container p-4">
+        <main className="p-4">
             <h1 className="mb-4 w-full text-center text-2xl">My Page</h1>
             <div className="flex items-center space-x-4">
                 <Avatar>
