44c55761d69f0ffc601ddcc52ddf1560ca4261f0
drive <driveasano@gmail.com>
2025-01-17 19:54:02 +0900

    Search機能を実装し、検索結果を表示するためにSearchCardsコンポーネントを追加

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { redirect } from 'next/navigation';
 import { createClient } from '@/utils/supabase/server';
-import GetAll from './_components/get-all';
+import { Search } from './_components/search';
+import SearchCards from './_components/search-result-cards';
 
-const Page = async () => {
+const Page = async ({
+    searchParams,
+}: {
+    searchParams: { keyword?: string };
+}) => {
+    // ユーザー認証
     const supabase = await createClient();
 
     const { data, error } = await supabase.auth.getUser();
     if (error || !data?.user) {
         redirect('/login');
     }
+
     return (
         <main className="p-4 pb-[112px]">
             <h1 className="mb-4 w-full text-center text-2xl">検索</h1>
-            <div>
-                <GetAll />
+            <Search />
+            <div className="mt-4">
+                <SearchCards keyword={searchParams.keyword || ''} />
             </div>
         </main>
     );
