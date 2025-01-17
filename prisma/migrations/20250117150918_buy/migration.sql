c706309263f0ff88dead7b0c598fdbdfeae0b25a
drive <driveasano@gmail.com>
2025-01-18 00:12:48 +0900

    「Buy」テーブルから「price」カラムを削除

---
+++ prisma/migrations/20250117150918_buy/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `price` on the `Buy` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Buy" DROP COLUMN "price";
