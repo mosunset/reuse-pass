dfc471d1191d0e520ae07a61b66d8a31e9722924
drive <driveasano@gmail.com>
2025-01-20 19:25:13 +0900

    チャット機能のためのコンポーネントを追加し、メッセージ履歴の表示と送信機能を実装

---
+++ src/components/chat/realtimemessage.tsx
@@ -0,0 +1,10 @@
+'use client';
+interface ChatProps {
+    buyId: number;
+    userId: string;
+}
+const RealTileMessage = ({ buyId, userId }: ChatProps) => {
+    return <div>RealTileMessage</div>;
+};
+
+export default RealTileMessage;
