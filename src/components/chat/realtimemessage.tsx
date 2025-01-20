049677abc1b53ea23169eb7a66da0d37b9d90b8b
drive <driveasano@gmail.com>
2025-01-20 20:57:21 +0900

    チャットコンポーネントにSupabaseのリアルタイムメッセージ受信機能を追加

--- src/components/chat/realtimemessage.tsx
+++ src/components/chat/realtimemessage.tsx
@@ -1,9 +1,33 @@
 'use client';
+import { useEffect } from 'react';
+import { createClient } from '@/utils/supabase/client';
+
 interface ChatProps {
     buyId: number;
     userId: string;
 }
 const RealTileMessage = ({ buyId, userId }: ChatProps) => {
+    const supabase = createClient();
+
+    useEffect(() => {
+        const channels = supabase
+            .channel('custom-all-channel')
+            .on(
+                'postgres_changes',
+                {
+                    event: '*',
+                    schema: 'public',
+                    table: 'Message',
+                },
+                (payload) => {
+                    console.log('Change received!', payload);
+                }
+            )
+            .subscribe();
+        return () => {
+            supabase.removeChannel(channels);
+        };
+    }, []);
     return <div>RealTileMessage</div>;
 };
 
