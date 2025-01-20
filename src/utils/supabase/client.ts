96c40f132ad36a8a08467c49384ca537f86bdb5a
drive <driveasano@gmail.com>
2025-01-20 20:23:32 +0900

    Supabaseクライアントの作成関数を追加し、JS用のクライアント作成関数を実装

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,15 @@
 import { createBrowserClient } from '@supabase/ssr';
+import { createClient as js } from '@supabase/supabase-js';
 
 export function createClient() {
     return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+}
+
+export function createClientJS() {
+    return js(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     );
