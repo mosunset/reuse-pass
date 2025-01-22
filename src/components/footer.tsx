424c8e83a89c636226ad384f92e13c9d7bd4d485
drive <driveasano@gmail.com>
2025-01-22 21:15:19 +0900

    フッターコンポーネントを非同期に変更し、ユーザー認証を追加。プレミアムユーザーでない場合に広告を表示。

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { GetUserByUserId } from '@/actions/users';
 import HeroiconsSolidCamera from '@/icons/HeroiconsSolidCamera';
 import HeroiconsSolidHome from '@/icons/HeroiconsSolidHome';
 import HeroiconsSolidMagnifyingGlass from '@/icons/HeroiconsSolidMagnifyingGlass';
+import { createClient } from '@/utils/supabase/server';
+import Ad from './ad';
 
-const Footer = () => {
+const Footer = async () => {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase.auth.getUser();
+    if (error || !data?.user) {
+        redirect('/login');
+    }
+    const user = await GetUserByUserId(data.user.id);
     return (
-        <footer className="fixed bottom-0 mt-4 block w-full min-w-[316px] max-w-[700px] bg-slate-200 p-4 pt-6">
-            <div className="flex justify-around">
-                <Link href="/search">
-                    <div className="flex min-w-[80px] flex-col items-center">
-                        <HeroiconsSolidMagnifyingGlass className="!h-8 !w-8" />
-                        <div>検索</div>
+        <>
+            {!user?.premium && (
+                <div className="fixed bottom-[100px] z-50 block w-full min-w-[316px] max-w-[700px]">
+                    <div className="mb-4 max-h-40 overflow-auto text-center text-sm text-slate-500">
+                        <Ad />
                     </div>
-                </Link>
-                <Link href="/sell">
-                    <div className="flex min-w-[80px] flex-col items-center">
-                        <HeroiconsSolidCamera className="!h-8 !w-8" />
-                        <div>出品</div>
-                    </div>
-                </Link>
-                <Link href="/mypage">
-                    <div className="flex min-w-[80px] flex-col items-center">
-                        <HeroiconsSolidHome className="!h-8 !w-8" />
-                        <div>マイページ</div>
-                    </div>
-                </Link>
-            </div>
-        </footer>
+                </div>
+            )}
+            <footer className="fixed bottom-0 mt-4 block w-full min-w-[316px] max-w-[700px] bg-slate-200 p-4 pt-6">
+                <div className="flex justify-around">
+                    <Link href="/search">
+                        <div className="flex min-w-[80px] flex-col items-center">
+                            <HeroiconsSolidMagnifyingGlass className="!h-8 !w-8" />
+                            <div>検索</div>
+                        </div>
+                    </Link>
+                    <Link href="/sell">
+                        <div className="flex min-w-[80px] flex-col items-center">
+                            <HeroiconsSolidCamera className="!h-8 !w-8" />
+                            <div>出品</div>
+                        </div>
+                    </Link>
+                    <Link href="/mypage">
+                        <div className="flex min-w-[80px] flex-col items-center">
+                            <HeroiconsSolidHome className="!h-8 !w-8" />
+                            <div>マイページ</div>
+                        </div>
+                    </Link>
+                </div>
+            </footer>
+        </>
     );
 };
 
