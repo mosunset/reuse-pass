656ad152c7011432782898cd20899f559fb14144
drive <driveasano@gmail.com>
2025-01-22 18:22:56 +0900

    プロフィール変更ページにユーザー認証を追加し、ユーザー情報を取得して表示する機能を実装

--- src/app/mypage/change-profile/page.tsx
+++ src/app/mypage/change-profile/page.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import { redirect } from 'next/navigation';
+import { GetUserByUserId } from '@/actions/users';
+import { createClient } from '@/utils/supabase/server';
+import ChangeProfile from './_components/change-profile';
 
-const Page = () => {
+const Page = async () => {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase.auth.getUser();
+    if (error || !data?.user) {
+        redirect('/login');
+    }
+
+    const user = await GetUserByUserId(data?.user.id);
     return (
         <main className="p-4 pb-[112px]">
             <h1 className="mb-4 w-full text-center text-2xl">
                 プロフィール変更
             </h1>
-            <div></div>
+            <div>
+                <ChangeProfile
+                    id={data?.user.id}
+                    name={user?.name || ''}
+                    bio={user?.bio || ''}
+                />
+            </div>
         </main>
     );
 };
